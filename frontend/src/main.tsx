35afdc5ab1fcf284be659039587698ebc75ee185
Aryan <aryan.tyagi202@gmail.com>
2026-06-02 16:39:43 +0530

    fix: remove unused leaflet-draw stylesheet import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import "leaflet/dist/leaflet.css";
-import "leaflet-draw/dist/leaflet.draw.css"
 import "./utils/leafletSetup"; // Fix Leaflet icons
 
 
