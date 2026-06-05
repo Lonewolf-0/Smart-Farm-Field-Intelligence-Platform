1a5c5746a18b497f3706f9574f15d885e85d6b9b
Aryan Tyagi <aryan.tyagi202@gmail.com>
2026-06-05 15:03:41 +0530

    feat: add SprayCalendar component to display and manage pesticide spray schedules

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -9,6 +9,7 @@ import CropSuitabilityCard from "../components/Dashboard/CropSuitabilityCard";
 import FertilizerCard from "../components/Dashboard/FertilizerCard";
 import NDVICard from "../components/Dashboard/NDVICard";
 import PesticideCard from "../components/Dashboard/PesticideCard";
+import SprayCalendar from "../components/Dashboard/SprayCalendar";
 
 function DashboardPage() {
   const [fields, setFields] = useState<Field[]>([]);
@@ -88,7 +89,12 @@ function DashboardPage() {
           <WeatherCard fieldId={selectedFieldId} />
           <CropSuitabilityCard fieldId={selectedFieldId} />
           <FertilizerCard fieldId={selectedFieldId} />
-          <PesticideCard fieldId={selectedFieldId} />
+          <div className="md:col-span-2">
+            <PesticideCard fieldId={selectedFieldId} />
+          </div>
+          <div className="md:col-span-2">
+            <SprayCalendar fieldId={selectedFieldId} />
+          </div>
         </div>
       )}
     </section>
